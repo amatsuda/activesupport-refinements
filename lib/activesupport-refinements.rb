3a28e6366d799199d9499065430e343aed54b8d8
Akira Matsuda <ronnie@dio.jp>
2012-10-31 15:14:17 +0900

    better to require individually for now

--- lib/activesupport-refinements.rb
+++ lib/activesupport-refinements.rb
@@ -1,6 +1,9 @@
 require 'activesupport-refinements/version'
+# need to load this in advance, because Module#remove_method is heavily used in AS
+require 'active_support/core_ext/module/remove_method'
 
-Dir[File.join(File.dirname(__FILE__), 'active_support/refinements/core_ext/*.rb')].sort.each do |path|
-  next if File.basename(path, '.rb') == 'logger'
-  require "active_support/refinements/core_ext/#{File.basename(path, '.rb')}"
-end
+# requiring all core_exts ends up with requiring many modules from the original AS. Commenting out for now
+# Dir[File.join(File.dirname(__FILE__), 'active_support/refinements/core_ext/*.rb')].sort.each do |path|
+#   next if File.basename(path, '.rb') == 'logger'
+#   require "active_support/refinements/core_ext/#{File.basename(path, '.rb')}"
+# end
